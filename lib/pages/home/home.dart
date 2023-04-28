613a18fc97b930bc7eeb465dd5cec74867ceb975
koliche <koliche.moahmed0@gmail.com>
2023-04-28 07:47:56 +0100

    home page ui

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -13,145 +13,223 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-      ),
-      body: Column(
-      children: <Widget>[
-        Container(
-          height: size.height * 0.2,
-          decoration: BoxDecoration(
-            color: Colors.green,
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(40.0),
-                bottomRight: Radius.circular(40.0)),
-          ),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
         ),
-
-        SizedBox(height: size.height * 0.05),
-
-        Column(
-
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: Column(
+          children: <Widget>[
+            Stack(
               children: [
-                InkWell(
-                  onTap: (){},
-                  child: Container(
-                    height: size.height*0.2,
-                    width: size.width*0.4,
-                    decoration: BoxDecoration(
-                      boxShadow: [BoxShadow(
-                          offset: Offset(0.0, 20.0),
-                          blurRadius: 30.0,
-                          color: Colors.black12
-                      )],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(22),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
+                Container(
+                  margin: EdgeInsets.all(15),
+                  padding: EdgeInsets.all(20),
+                  // height: size.height * 0.1,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
                       Container(
-                          child: Image(image: AssetImage('assets/images/farmer.png')),
-                      height: size.height*0.15,),
-                      Center(child: Text("hello",style: TextStyle(),))
-                    ]
-                    ),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[800], shape: BoxShape.circle),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Go Premium',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Get access to \nthe fast and accurate models!",
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                          )
+                        ],
+                      )
+                    ],
                   ),
                 ),
-
-                InkWell(
-                  onTap: (){},
+                Positioned(
+                  bottom: 15,
+                  right: 15,
                   child: Container(
-                    height: size.height*0.2,
-                    width: size.width*0.4,
+                    padding: EdgeInsets.all(10),
                     decoration: BoxDecoration(
-                      boxShadow: [BoxShadow(
-                          offset: Offset(0.0, 20.0),
-                          blurRadius: 30.0,
-                          color: Colors.black12
-                      )],
+                      color: Colors.grey[800],
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Icon(
+                      Icons.arrow_forward,
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(22),
-                    ),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            child: Image(image: AssetImage('assets/images/farmer.png')),
-                            height: size.height*0.15,),
-                          Center(child: Text("hello",style: TextStyle(),))
-                        ]
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: size.height*0.05),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                InkWell(
-                  onTap: (){},
-                  child: Container(
-                    height: size.height*0.2,
-                    width: size.width*0.4,
-                    decoration: BoxDecoration(
-                      boxShadow: [BoxShadow(
-                          offset: Offset(0.0, 20.0),
-                          blurRadius: 30.0,
-                          color: Colors.black12
-                      )],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(22),
-                    ),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            child: Image(image: AssetImage('assets/images/farmer.png')),
-                            height: size.height*0.15,),
-                          Center(child: Text("hello",style: TextStyle(),))
-                        ]
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: (){},
-                  child: Container(
-                    height: size.height*0.2,
-                    width: size.width*0.4,
-                    decoration: BoxDecoration(
-                      boxShadow: [BoxShadow(
-                          offset: Offset(0.0, 20.0),
-                          blurRadius: 30.0,
-                          color: Colors.black12
-                      )],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(22),
-                    ),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            child: Image(image: AssetImage('assets/images/farmer.png')),
-                            height: size.height*0.15,),
-                          Center(child: Text("hello",style: TextStyle(),))
-                        ]
                     ),
                   ),
                 )
               ],
             ),
-            SizedBox(height: size.height*0.03),
-
-
+            SizedBox(height: size.height * 0.05),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        height: size.height * 0.2,
+                        width: size.width * 0.4,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                offset: Offset(0.0, 20.0),
+                                blurRadius: 30.0,
+                                color: Colors.black12)
+                          ],
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(22),
+                        ),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                child: Image(
+                                    image:
+                                        AssetImage('assets/icons/workspace.png')),
+                                height: size.height * 0.15,
+                              ),
+                              Center(
+                                  child: Text(
+                                "Workspace",
+                                style: TextStyle(),
+                              ))
+                            ]),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        height: size.height * 0.2,
+                        width: size.width * 0.4,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                offset: Offset(0.0, 20.0),
+                                blurRadius: 30.0,
+                                color: Colors.black12)
+                          ],
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(22),
+                        ),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                child: Image(
+                                    image:
+                                        AssetImage('assets/icons/yield.png')),
+                                height: size.height * 0.15,
+                              ),
+                              Center(
+                                  child: Text(
+                                "Yield Estimation",
+                                style: TextStyle(),
+                              ))
+                            ]),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: size.height * 0.05),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        height: size.height * 0.2,
+                        width: size.width * 0.4,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                offset: Offset(0.0, 20.0),
+                                blurRadius: 30.0,
+                                color: Colors.black12)
+                          ],
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(22),
+                        ),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                child: Image(
+                                    image:
+                                        AssetImage('assets/icons/detection.png')),
+                                height: size.height * 0.15,
+                              ),
+                              Center(
+                                  child: Text(
+                                "Object Detection",
+                                style: TextStyle(),
+                              ))
+                            ]),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        height: size.height * 0.2,
+                        width: size.width * 0.4,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                offset: Offset(0.0, 20.0),
+                                blurRadius: 30.0,
+                                color: Colors.black12)
+                          ],
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(22),
+                        ),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                child: Image(
+                                    image:
+                                        AssetImage('assets/icons/segmentation.png')),
+                                height: size.height * 0.15,
+                              ),
+                              Center(
+                                  child: Text(
+                                "Segmantation",
+                                style: TextStyle(),
+                              ))
+                            ]),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: size.height * 0.03),
+              ],
+            ),
           ],
-        ),
-      ],
-    )
-    );
+        ));
   }
 }
